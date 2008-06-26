c91b2700c71415529d620752588295c3e9053680
mark burdett <mfburdett@gmail.com>
2008-06-26 22:34:18 +0000

    refactoring (move html and themeable functions to templates)

---
+++ google_cse_results.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+?>
+
+<?php print $results_searchbox_form; ?>
+
+<div id="google-cse-results">
+  <noscript>
+    <?php print $noscript; ?>
+  </noscript>
+</div>
+
+<script type="text/javascript">
+  //<![CDATA[
+    var googleSearchIframeName = 'google-cse-results';
+    var googleSearchFormName = 'google-cse-results-searchbox-form';
+    var googleSearchFrameWidth = Drupal.settings.googleCSE.resultsWidth;
+    var googleSearchFrameborder = 0;
+    var googleSearchDomain = Drupal.settings.googleCSE.domain;
+    var googleSearchPath = '/cse';
+  //]]>
+</script>
+
+<script type="text/javascript" src="http://www.google.com/afsonline/show_afs_search.js"></script>
